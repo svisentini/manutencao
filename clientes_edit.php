b6012bd26e5a2634d90fc63a960855b0186ae98d
Sidao <svisentini@gmail.com>
2022-07-26 23:15:17 -0300

    test action

--- clientes_edit.php
+++ clientes_edit.php
@@ -102,7 +102,7 @@
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Cadastro de Clientes</h3>
+                            <h3>Cadastro Clientes</h3>
                             
                         </div>
 
